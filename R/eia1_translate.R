5d5ce0689323628e100cfd063a778099cbc498bd
Jameel Alsalam <jalsalam@gmail.com>
2023-02-01 12:59:16 -0500

    initial translation function for v1 IDs

---
+++ R/eia1_translate.R
@@ -0,0 +1,36 @@
+
+#' Convert API version 1 series IDs to version 2 request specifications
+#'
+#' @param series_id character vector of series IDs
+#'
+#TODO: should these be returned as httr2 reqeust objects? or as a different data format?
+#'
+#' @return list of v2 request specifications (same format as returned by API)
+#'
+#' @examples
+#' series_id <- c("TOTAL.TECCEUS.A", "TOTAL.TERCEUS.A")
+#' @export
+eia1_translate <- function(series_id) {
+
+  seriesid_routes <- paste("seriesid", series_id, sep = "/")
+
+  # limit to 1 / second
+  list_reqs <- purrr::map(seriesid_routes, ~eia2_req(.x) |> req_throttle(rate = 1))
+
+  list_resps <- list()
+  for (i in seq_along(list_reqs)) {
+    print(list_reqs[[i]]$url)
+    list_resps[[i]] <- req_perform(list_reqs[[i]])
+  }
+
+  list_request_spec <- purrr::map(list_resps, function(resp) {
+    body_list <- resp |> resp_body_string() |> jsonlite::fromJSON()
+    body_list$request
+  })
+
+  list_request_spec
+}
+
+
+
+
