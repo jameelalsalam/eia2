d36118d210fdbc1d32b05d98395a64a087632c26
Jameel Alsalam <jalsalam@gmail.com>
2023-03-22 10:16:56 -0400

    update scripts, example >5000

--- scripts/examples.R
+++ scripts/examples.R
@@ -2,8 +2,8 @@
 
 # scratchwork examples
 
-devtools::load_all(".")
-
+#devtools::load_all(".")
+library(eia2)
 
 # Walking through the sections of the documentation:
 
@@ -12,29 +12,37 @@ devtools::load_all(".")
 elec_meta <- eia2("electricity")
 
 # info on possible routes below /electricity:
-elec_meta
+elec_meta$routes
 
 # below /electricity/retail-sales
 elec_retail_meta <- eia2("electricity/retail-sales")
 
 # this time no routes below this:
-elec_retail_meta |> eia2_resp_meta_routes() |> View()
+elec_retail_meta$routes
 
 
 ##### Examining a Metadata request:
 # DOC SECTION: https://www.eia.gov/opendata/documentation.php#Examining
 
 # so what is returned?
-elec_retail_meta |> resp_body_json() |> str()
+elec_retail_meta
 
 # info on frequency, facets, startPerid, endPeriod, and data columns are returned
 
 ##### Facets and their available values:
 # DOC SECTION: https://www.eia.gov/opendata/documentation.php#FacetValues
 
-elec_retail_facet_sector <- eia2("electricity/retail-sales/facet/sectorid")
+# you can ask about possible facet values either by constructing manually
+elec_retail_facet_meta <- eia2("electricity/retail-sales/facet/sectorid")
 
-elec_retail_facet_sector |> resp_body_json() |> str()
+elec_retail_facet_meta
+
+# or with purpose-built function:
+elec_retail_facet_sector <- eia2_facet("electricity/retail-sales", facet = "sectorid")
+
+# the only difference right now is slightly different data extraction format
+
+elec_retail_facet_sector
 # we are told 6 possible values for facet sectorid: RES, COM, ALL, TRA, OTH, IND
 # we can query on a sectorid not in this list, for example xxx. The API won't return
 # an error--because that's a valid query. However, we won't receive any data
@@ -46,20 +54,33 @@ elec_retail_facet_sector |> resp_body_json() |> str()
 elec_retail_data_v1 <- eia2("electricity/retail-sales/data")
 View(elec_retail_data_v1)
 
-# from the response you can get total, but its gone now:
-# eia2_resp_total(elec_retail_data_v1)
+# the function `eia2()` is meant to be user-friendly, so it automatically extracts
+# data from a data request response.
 
-# [1] 97464
-# API doc shows 7440. Maybe this is restricted to CO?
+# from the unextracted response, you could get the total, but its not returned by `eia2()`
+# another option is to control each of the steps yourself:
 
+# 1) construct request object
+elec_retail_req <- eia2_req("electricity/retail-sales/data")
+
+# 2) perform request
+elec_retail_resp <- eia2_req_perform(elec_retail_req)
+
+# 3) extract information as needed. Check total rows, and then look at first 5000
+
+eia2:::eia2_resp_total(elec_retail_resp)
+# [1] 98208
+
+eia2_resp_data(elec_retail_resp)
+
+
+## Restrict now to a smaller set, CO only
 elec_retail_CO <- eia2("electricity/retail-sales", facets = list(stateid = "CO"))
-elec_retail_CO |> resp_body_json() |> str()
+elec_retail_CO
 
+# to retrieve data, either need to add /data to route, or specify data_cols
 elec_retail_data_CO <- eia2("electricity/retail-sales/data", facets = list(stateid = "CO"))
-
-
-
-
+elec_retail_data_CO
 
 elec_retail_sales_annual_data <-
   eia2("electricity/retail-sales", frequency = "annual", data_cols = c("revenue"))
---
+++ scripts/more-than-5000.R
@@ -0,0 +1,47 @@
+# example-more-than-5000.R
+
+# The EIA API will only return 5000 data points at a time (in json, in XML only 300).
+# For data queries with more data than this, you can make requests in a loop.
+# Right now for this approach, you need to control the steps individually.
+
+eia2("electricity/retail-sales")
+
+req_big <- eia2_req(
+  "electricity/retail-sales",
+  data_cols = "revenue",
+  facets = list(
+    stateid = c("CO", "CA", "TX", "NH", "MN", "VA", "IL", "MD", "AK")
+  ))
+
+resp_big_1 <- eia2_req_perform(req_big)
+
+total_n <- resp_big_1 |> eia2:::eia2_resp_total()
+total_n
+
+resp_big_data <- list()
+resp_big_data[[1]] <- eia2_resp_data(resp_big_1)
+
+if (total_n > 5000) {
+  for (offset in seq(from = 5000, to = total_n, by = 5000)) {
+    i <- (offset %/% 5000) + 1
+    resp_big_data[[i]] <- {
+      eia2_req(
+        "electricity/retail-sales",
+        data_cols = "revenue",
+        facets = list(
+          stateid = c("CO", "CA", "TX", "NH", "MN", "VA", "IL", "MD", "AK")
+        ),
+        offset = offset
+      ) |>
+        eia2_req_perform() |>
+        eia2_resp_data()
+    }
+  }
+}
+
+data <- dplyr::bind_rows(!!!resp_big_data)
+
+nrow(data)
+total_n
+
+
